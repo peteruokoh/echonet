a96a1fc153bbbe6734634e915923797c8b2bc79d
Pete Okoh <peter.okoh@yahoo.com>
2025-10-02 02:28:12 +0100

    Updated Setting File

--- echonet/settings.py
+++ echonet/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'echonet.wsgi.application'
 # }
 
 DATABASES = {
-    "default": dj_database_url.config(default="sqlite:///db.sqlite3", conn_max_age=600)
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
